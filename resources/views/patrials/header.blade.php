335905fbc18485d66a6a7a6210052f104bc83afc
Jero1304 <calogeroleto13@gmail.com>
2023-04-08 15:19:24 +0200

    ADD navbar

---
+++ resources/views/patrials/header.blade.php
@@ -0,0 +1,61 @@
+<header>
+    <div class="container">
+        <div class="header-bar">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            <nav>
+                <ul class="menu">
+                    <li class="menu-item">charter</li>
+                    <li class="menu-item">comics</li>
+                    <li class="menu-item">movies</li>
+                    <li class="menu-item">TV</li>
+                    <li class="menu-item">collectibles</li>
+                    <li class="menu-item">videos</li>
+                    <li class="menu-item">fans</li>
+                    <li class="menu-item">news</li>
+                    <li class="menu-item">shop</li>
+                    <li class="menu-item nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            shop
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">SHOP 1</a></li>
+                            <li><a class="dropdown-item" href="#">SHOP 1</a></li>
+                            <li><a class="dropdown-item" href="#">SHOP 1</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <form class="header-search" role="search">
+                <input class="search form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn" type="submit">O</button>
+            </form>
+
+        </div>
+    </div>
+</header>
+
+{{-- <script>
+import HeaderMenu from "./HeaderMenu.vue"
+export default {
+    components: {
+        HeaderMenu
+    },
+    data() {
+        return {
+            options: [
+                    'charter',
+                    'comics',
+                    'movies',
+                    'TV',
+                    'games',
+                    'collectibles',
+                    'videos',
+                    'fans',
+                    'news',
+                    'shop'
+                ]
+        }
+    }
+} 
+</script> --}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,17 +7,57 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
     @vite('resources/js/app.js')
 
 </head>
 
 <body>
+    @extends('layouts.app')
+    
 
-    <main class="bg-light">
+    @section('content')
+        <h1>content</h1>
+    @endsection
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- <!DOCTYPE  html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+</head>
+
+<body> --}}
+
+{{-- <main class="bg-light">
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -40,8 +80,8 @@
             </div>
 
         </div>
-    </main>
+    </main> --}}
 
-</body>
+{{-- </body>
 
-</html>
+</html> --}}
